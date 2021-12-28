a103e48c977ef85d1813901703071d49af652833
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-27 22:56:35 -0300

    Add Notebook.java

---
+++ Computer/Notebook.java
@@ -0,0 +1,25 @@
+public class Notebook extends Computer {
+
+	public Notebook() {
+		
+	}
+	
+	public void inputScreen() {
+		
+		super.inputScreen();
+	}
+	
+	public void inputColor() {
+		
+		super.inputColor();
+	}
+	
+	public void defineTelaCor() {
+		inputScreen();
+		inputColor();
+	}
+	
+	public void informaCaracteristicas() {
+		super.informaCaracteristicas();
+	}
+}
