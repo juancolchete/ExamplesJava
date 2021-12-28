614b6a7022a1cd939b2f9301c62955c083306b21
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-28 07:23:40 -0300

    Add Agua.java

---
+++ Chemics/Agua.java
@@ -0,0 +1,14 @@
+package Questao2;
+
+public class Agua extends SubstanciaPura{
+	public void entrarPontoDeEbulicao(double temp) {
+		if(temp >= 100) {
+			System.out.println("Entrou em ebulição");
+		}
+	}
+	public void entrarPontoDeFusao(double temp) {
+		if(temp >= 0) {
+			System.out.println("Entrou em fusão");
+		}
+	}
+}
