74d83f1e03b180164cc0a8989faafe06279b381a
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-27 22:56:15 -0300

    Add Desktop.java

---
+++ Computer/Desktop.java
@@ -0,0 +1,25 @@
+public class Desktop extends Computer{
+
+	public Desktop() {
+		
+	}
+	
+	public void inputScreen() {
+		
+		super.inputScreen();
+	}
+	
+	public void inputColor() {
+		
+		super.inputColor();
+	}
+	
+	public void defineTelaCor() {
+		inputScreen();
+		inputColor();
+	}
+	
+	public void informaCaracteristicas() {
+		super.informaCaracteristicas();
+	}
+}
