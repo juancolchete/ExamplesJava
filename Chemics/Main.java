54fe34b92bed38b90608878e70c636bc60616386
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-28 07:35:18 -0300

    Rename call of startFusionPoint and startBoilingPoint in main

--- Chemics/Main.java
+++ Chemics/Main.java
@@ -1,25 +1,25 @@
 public class Main {
 	public static void main(String[] args) {
 		SubstanciaPura objsubpura = new SubstanciaPura();
-		objsubpura.entrarPontoDeEbulicao(100);
-		objsubpura.entrarPontoDeFusao(100);
+		objsubpura.startBoilingPoint(100);
+		objsubpura.startFusionPoint(100);
 		
 		Agua objagua = new Agua();
-		objagua.entrarPontoDeEbulicao(100);
-		objagua.entrarPontoDeFusao(0);
-		objagua.entrarPontoDeEbulicao(99);
-		objagua.entrarPontoDeFusao(-1);
+		objagua.startBoilingPoint(100);
+		objagua.startFusionPoint(0);
+		objagua.startBoilingPoint(99);
+		objagua.startFusionPoint(-1);
 		
 		Amonia objamonia = new Amonia();
-		objamonia.entrarPontoDeEbulicao(-33.34);
-		objamonia.entrarPontoDeFusao(-77.73);
-		objamonia.entrarPontoDeEbulicao(-33.35);
-		objamonia.entrarPontoDeFusao(-77.74);
+		objamonia.startBoilingPoint(-33.34);
+		objamonia.startFusionPoint(-77.73);
+		objamonia.startBoilingPoint(-33.35);
+		objamonia.startFusionPoint(-77.74);
 		
 		Alcool objalcool = new Alcool();
-		objalcool.entrarPontoDeEbulicao(78.4);
-		objalcool.entrarPontoDeFusao(0);
-		objalcool.entrarPontoDeEbulicao(78.3);
-		objalcool.entrarPontoDeFusao(-1);
+		objalcool.startBoilingPoint(78.4);
+		objalcool.startFusionPoint(0);
+		objalcool.startBoilingPoint(78.3);
+		objalcool.startFusionPoint(-1);
 	}
 }
