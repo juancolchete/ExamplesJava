837c22ccae0032754acbb379e10030e80723030b
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-27 23:11:59 -0300

    Fix instancy of classes

--- EletronicEquipament/Main.java
+++ EletronicEquipament/Main.java
@@ -6,13 +6,13 @@ public class Main {
 		
 		Eletrodomestic eletroD = new Eletrodomestic("Micro Wave","Philco",10.0,200.0,500,false,"Kitchen");
 		
-		Eletromecanicos eletroM = new Eletromecanicos("Escalator","Philco",10.0,200.0,500,false,520.0);
+		EletricalMecanic eletroM = new EletricalMecanic("Escalator","Philco",10.0,200.0,500,false,520.0);
 		
 		Hydraulic hidro = new Hydraulic("Jack","3M",10.0,200.0,500,false,20.0);
 		
 		WaterTap torneira = new WaterTap("3M","3M",10.0,200.0,500,false,566, "Plastic");
 		
-		MotoresEletricos motoresE = new MotoresEletricos("Tesla engine","Tesla",10.0,200.0,500,false,520.0, 1233);
+		EletricalEngines motoresE = new EletricalEngines("Tesla engine","Tesla",10.0,200.0,500,false,520.0, 1233);
 		
 		TV tv = new TV("TV LG","Philco",10.0,200.0,500,false,"Eletrodomestic", 0);
 		
