8419f186f528415c5190c6c7fa9387be5fdbb66b
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-28 07:41:14 -0300

    Ammonia.java extends PureSubstance

--- Chemics/Ammonia.java
+++ Chemics/Ammonia.java
@@ -1,4 +1,4 @@
-public class Amonia extends SubstanciaPura{
+public class Amonia extends PureSubstance{
 	public void startBoilingPoint(double temp) {
 		if(temp >= -33.34) {
 			System.out.println("Entered in bolling point");
