304e9037b9f7555699ad460368cfc7bdb42b2710
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-27 22:56:45 -0300

    Add Ultrabook.java

---
+++ Computer/Ultrabook.java
@@ -0,0 +1,26 @@
+public class Ultrabook extends Computer{
+
+	
+	public Ultrabook() {
+		
+	}	
+	
+	public void inputScreen() {
+		
+		super.inputScreen();
+	}
+	
+	public void inputColor() {
+		
+		super.inputColor();
+	}
+	
+	public void defineTelaCor() {
+		inputScreen();
+		inputColor();
+	}
+	
+	public void informaCaracteristicas() {
+		super.informaCaracteristicas();
+	}
+}
