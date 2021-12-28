430d185ebc621dff39ddd0f204ecdc5bb2d119c4
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-28 07:43:32 -0300

    Rename temp to temperature Water.java

--- Chemics/Water.java
+++ Chemics/Water.java
@@ -1,11 +1,11 @@
 public class Agua extends PureSubstance{
-	public void startBoilingPoint(double temp) {
-		if(temp >= 100) {
+	public void startBoilingPoint(double temperature) {
+		if(temperature >= 100) {
 			System.out.println("Entered in bolling point");
 		}
 	}
-	public void startFusionPoint(double temp) {
-		if(temp >= 0) {
+	public void startFusionPoint(double temperature) {
+		if(temperature >= 0) {
 			System.out.println("Entered in fusion point");
 		}
 	}
