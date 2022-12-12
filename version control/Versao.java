0903a05795e2e298cf78e144c1b01bf22ed592e0
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2022-12-11 22:08:12 -0300

    feat: Versao.java

---
+++ version control/Versao.java
@@ -0,0 +1,9 @@
+
+public class Versao {
+	int identificador;
+	Diretorio diretorio;
+	Versao(int identificador, Diretorio diretorio){
+		this.identificador = identificador;
+		this.diretorio = diretorio;
+	}
+}
